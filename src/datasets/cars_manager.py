Fix columns name and scaling for full_features datasets

src/datasets/cars_manager.py
@@ -41,7 +41,8 @@ class CarsManager(AbstractManager):
         df = pd.read_csv(filepath).replace({'.': np.nan})
         df = clean_dataframe(df, CarsManager.FEATURES)
         if full_features:
-            df = pd.get_dummies(df).dropna()
+            df['manufacturer'] = df['manufacturer'].map(lambda v: v.strip())
+            df = pd.get_dummies(df, prefix_sep='/').dropna()
         else:
             df = df[['price', 'sales']].dropna()
         extrapolation = {'price': 0.2} if extrapolation else None
@@ -58,6 +59,7 @@ class CarsManager(AbstractManager):
             x_scaling = {v.alias or k: x_scaling for k, v in CarsManager.FEATURES.items() if v.kind == 'float'}
         elif full_grid:
             grid = pd.DataFrame.from_dict({'price': np.linspace(self.bound[0], self.bound[1], 700)})
+            x_scaling = {'price': x_scaling}
         super(CarsManager, self).__init__(
             directions={'price': -1},
             stratify=False,

src/datasets/default_manager.py
@@ -25,24 +25,24 @@ class DefaultManager(AbstractManager):
         'EDUCATION': FeatureInfo(kind='category', alias='education'),
         'MARRIAGE': FeatureInfo(kind='category', alias='marriage'),
         'AGE': FeatureInfo(kind='float', alias='age'),
-        'PAY_0': FeatureInfo(kind='float', alias='sep status'),
-        'PAY_2': FeatureInfo(kind='float', alias='aug status'),
-        'PAY_3': FeatureInfo(kind='float', alias='jul status'),
-        'PAY_4': FeatureInfo(kind='float', alias='jun status'),
-        'PAY_5': FeatureInfo(kind='float', alias='may status'),
-        'PAY_6': FeatureInfo(kind='float', alias='apr status'),
-        'BILL_AMT1': FeatureInfo(kind='float', alias='sep bill'),
-        'BILL_AMT2': FeatureInfo(kind='float', alias='aug bill'),
-        'BILL_AMT3': FeatureInfo(kind='float', alias='jul bill'),
-        'BILL_AMT4': FeatureInfo(kind='float', alias='jun bill'),
-        'BILL_AMT5': FeatureInfo(kind='float', alias='may bill'),
-        'BILL_AMT6': FeatureInfo(kind='float', alias='apr bill'),
-        'PAY_AMT1': FeatureInfo(kind='float', alias='sep payment'),
-        'PAY_AMT2': FeatureInfo(kind='float', alias='aug payment'),
-        'PAY_AMT3': FeatureInfo(kind='float', alias='jul payment'),
-        'PAY_AMT4': FeatureInfo(kind='float', alias='jun payment'),
-        'PAY_AMT5': FeatureInfo(kind='float', alias='may payment'),
-        'PAY_AMT6': FeatureInfo(kind='float', alias='apr payment')
+        'PAY_0': FeatureInfo(kind='float', alias='sep_status'),
+        'PAY_2': FeatureInfo(kind='float', alias='aug_status'),
+        'PAY_3': FeatureInfo(kind='float', alias='jul_status'),
+        'PAY_4': FeatureInfo(kind='float', alias='jun_status'),
+        'PAY_5': FeatureInfo(kind='float', alias='may_status'),
+        'PAY_6': FeatureInfo(kind='float', alias='apr_status'),
+        'BILL_AMT1': FeatureInfo(kind='float', alias='sep_bill'),
+        'BILL_AMT2': FeatureInfo(kind='float', alias='aug_bill'),
+        'BILL_AMT3': FeatureInfo(kind='float', alias='jul_bill'),
+        'BILL_AMT4': FeatureInfo(kind='float', alias='jun_bill'),
+        'BILL_AMT5': FeatureInfo(kind='float', alias='may_bill'),
+        'BILL_AMT6': FeatureInfo(kind='float', alias='apr_bill'),
+        'PAY_AMT1': FeatureInfo(kind='float', alias='sep_payment'),
+        'PAY_AMT2': FeatureInfo(kind='float', alias='aug_payment'),
+        'PAY_AMT3': FeatureInfo(kind='float', alias='jul_payment'),
+        'PAY_AMT4': FeatureInfo(kind='float', alias='jun_payment'),
+        'PAY_AMT5': FeatureInfo(kind='float', alias='may_payment'),
+        'PAY_AMT6': FeatureInfo(kind='float', alias='apr_payment')
     }
     MARKERS = {k: v for k, v in enumerate(['o', 's', '^', '+'])}
 
@@ -52,11 +52,11 @@ class DefaultManager(AbstractManager):
         df = pd.read_csv(filepath)
         df = clean_dataframe(df, DefaultManager.FEATURES)
         if full_features:
-            df = pd.get_dummies(df).dropna()
+            df = pd.get_dummies(df, prefix_sep='/').dropna()
         else:
             df['marriage'] = df['marriage'].astype('float') - 1
             df = df[np.in1d(df['marriage'], [0, 1])]
-            df = df[['marriage', 'sep status', 'default']].dropna()
+            df = df[['marriage', 'sep_status', 'default']].dropna()
         return split_dataset(df, test_size=1 - train_fraction, val_size=0.0, stratify=df['default'])
 
     def __init__(self, filepath: str, full_features: bool = False, full_grid: bool = False, grid_augmented: int = 10,
@@ -65,15 +65,17 @@ class DefaultManager(AbstractManager):
         if full_features:
             assert full_grid is False, "'full_grid' is not supported with 'full_features'"
             self.months = ['sep', 'aug', 'jul', 'jun', 'may', 'apr']
+            x_scaling = {v.alias or k: x_scaling for k, v in DefaultManager.FEATURES.items() if v.kind == 'float'}
         else:
             self.months = ['sep']
+            x_scaling = {'sep_status': x_scaling}
             if full_grid:
                 marriage, status = np.meshgrid([0, 1], np.arange(-2, 9))
-                grid = pd.DataFrame.from_dict({'marriage': marriage.flatten(), 'sep status': status.flatten()})
+                grid = pd.DataFrame.from_dict({'marriage': marriage.flatten(), 'sep_status': status.flatten()})
         super(DefaultManager, self).__init__(
-            directions={f'{c} status': 1 for c in self.months},
+            directions={f'{c}_status': 1 for c in self.months},
             stratify=True,
-            x_scaling={'sep status': x_scaling},
+            x_scaling=x_scaling,
             y_scaling=None,
             label='default',
             loss=log_loss,
@@ -93,7 +95,7 @@ class DefaultManager(AbstractManager):
 
     def _get_sampling_functions(self, rng: Rng, num_augmented: Augmented = 7) -> SamplingFunctions:
         num_augmented = np.round(num_augmented / np.sqrt(len(self.months))).astype(int)
-        return {f'{m} status': (num_augmented, lambda s: rng.choice(np.arange(-2, 9), size=s)) for m in self.months}
+        return {f'{m}_status': (num_augmented, lambda s: rng.choice(np.arange(-2, 9), size=s)) for m in self.months}
 
     def _data_plot(self, figsize: Figsize, tight_layout: TightLayout, **kwargs):
         _, axes = plt.subplots(len(kwargs), 1, sharex='col', sharey='col', figsize=figsize, tight_layout=tight_layout)

src/datasets/law_manager.py
@@ -23,9 +23,9 @@ class LawManager(AbstractManager):
         'pass_bar': FeatureInfo(kind='float', alias='pass'),
         'lsat': FeatureInfo(kind='float', alias='lsat'),
         'ugpa': FeatureInfo(kind='float', alias='ugpa'),
-        'decile1': FeatureInfo(kind='float', alias='first year decile'),
-        'decile3': FeatureInfo(kind='float', alias='third year decile'),
-        'fam_inc': FeatureInfo(kind='float', alias='family income'),
+        'decile1': FeatureInfo(kind='float', alias='first_year_decile'),
+        'decile3': FeatureInfo(kind='float', alias='third_year_decile'),
+        'fam_inc': FeatureInfo(kind='float', alias='family_income'),
         'gender': FeatureInfo(kind='category', alias='gender'),
         'race1': FeatureInfo(kind='category', alias='race'),
         'cluster': FeatureInfo(kind='category', alias='prestige'),
@@ -38,7 +38,7 @@ class LawManager(AbstractManager):
         df = pd.read_csv(filepath)
         df = clean_dataframe(df, LawManager.FEATURES)
         if full_features:
-            df = pd.get_dummies(df).dropna()
+            df = pd.get_dummies(df, prefix_sep='/').dropna()
         else:
             df = df[['lsat', 'ugpa', 'pass']].dropna()
         return split_dataset(df, test_size=1 - train_fraction, val_size=0.0, stratify=df['pass'])
@@ -48,9 +48,11 @@ class LawManager(AbstractManager):
         grid = None
         if full_features:
             assert full_grid is False, "'full_grid' is not supported with 'full_features'"
+            x_scaling = {v.alias or k: x_scaling for k, v in LawManager.FEATURES.items() if v.kind == 'float'}
         elif full_grid:
             lsat, ugpa = np.meshgrid(np.linspace(0, 50, 64), np.linspace(0, 4, 64))
             grid = pd.DataFrame.from_dict({'lsat': lsat.flatten(), 'ugpa': ugpa.flatten()})
+            x_scaling = {'lsat': x_scaling, 'ugpa': x_scaling}
         super(LawManager, self).__init__(
             directions={'lsat': 1, 'ugpa': 1},
             stratify=True,

test/experimental/pipelines.py
@@ -30,6 +30,9 @@ MODELS: Dict[str, Tuple[Callable, Type]] = {
 
 
 class TestPipelines(unittest.TestCase):
+    def _grid_ground(self) -> Optional[int]:
+        raise NotImplementedError("please implement method '_grid_ground()'")
+
     def _model_parameters(self, model: str, dataset: str) -> Optional[Dict]:
         raise NotImplementedError("please implement method '_model_parameters()'")
 
@@ -44,10 +47,10 @@ class TestPipelines(unittest.TestCase):
         dataset, model = caller_info[-2], caller_info[-1]
         if 'univariate' in caller_function_name:
             dataset, model = f'{dataset} univariate', 'sbr univariate' if model == 'sbr' else model
+        expected_factory, expected_manager = DATASETS[dataset]
+        handler_routine, expected_handler = MODELS[model]
         # noinspection PyBroadException
         try:
-            expected_factory, expected_manager = DATASETS[dataset]
-            handler_routine, expected_handler = MODELS[model]
             # we can now use these information to retrieve the correct pipeline
             factory, _ = DatasetFactory(res_folder=RES_FOLDER).get_dataset(name=dataset, **kwargs)
             self.assertIsInstance(factory, expected_factory)
@@ -57,8 +60,8 @@ class TestPipelines(unittest.TestCase):
             self.assertIsInstance(handler, expected_handler)
             # noinspection PyUnresolvedReferences
             handler.test(summary_args=self._summary_args(model, dataset))
-        except Exception as exception:
-            self.fail(f'Arguments: {kwargs}\n{exception}')
+        except Exception:
+            self.fail(f'Arguments: {kwargs}')
 
     def test_meta_tests(self):
         datasets, models = np.meshgrid(
@@ -159,40 +162,40 @@ class TestPipelines(unittest.TestCase):
 
     def test_default_mlp(self):
         self._test(data_args=dict(full_features=False, full_grid=True))
-        self._test(data_args=dict(full_features=False, full_grid=False, grid_ground=100))
-        self._test(data_args=dict(full_features=True, full_grid=False, grid_ground=100))
+        self._test(data_args=dict(full_features=False, full_grid=False, grid_ground=self._grid_ground()))
+        self._test(data_args=dict(full_features=True, full_grid=False, grid_ground=self._grid_ground()))
 
     def test_default_sbr(self):
         self._test(data_args=dict(full_features=False, full_grid=True))
-        self._test(data_args=dict(full_features=False, full_grid=False, grid_ground=100))
-        self._test(data_args=dict(full_features=True, full_grid=False, grid_ground=100))
+        self._test(data_args=dict(full_features=False, full_grid=False, grid_ground=self._grid_ground()))
+        self._test(data_args=dict(full_features=True, full_grid=False, grid_ground=self._grid_ground()))
 
     def test_default_tfl(self):
         self._test(data_args=dict(full_features=False, full_grid=True))
-        self._test(data_args=dict(full_features=False, full_grid=False, grid_ground=100))
-        self._test(data_args=dict(full_features=True, full_grid=False, grid_ground=100))
+        self._test(data_args=dict(full_features=False, full_grid=False, grid_ground=self._grid_ground()))
+        self._test(data_args=dict(full_features=True, full_grid=False, grid_ground=self._grid_ground()))
 
     def test_default_mt(self):
         self._test(data_args=dict(full_features=False, full_grid=True))
-        self._test(data_args=dict(full_features=False, full_grid=False, grid_ground=100))
-        self._test(data_args=dict(full_features=True, full_grid=False, grid_ground=100))
+        self._test(data_args=dict(full_features=False, full_grid=False, grid_ground=self._grid_ground()))
+        self._test(data_args=dict(full_features=True, full_grid=False, grid_ground=self._grid_ground()))
 
     def test_law_mlp(self):
         self._test(data_args=dict(full_features=False, full_grid=True))
-        self._test(data_args=dict(full_features=False, full_grid=False, grid_ground=100))
-        self._test(data_args=dict(full_features=True, full_grid=False, grid_ground=100))
+        self._test(data_args=dict(full_features=False, full_grid=False, grid_ground=self._grid_ground()))
+        self._test(data_args=dict(full_features=True, full_grid=False, grid_ground=self._grid_ground()))
 
     def test_law_sbr(self):
         self._test(data_args=dict(full_features=False, full_grid=True))
-        self._test(data_args=dict(full_features=False, full_grid=False, grid_ground=100))
-        self._test(data_args=dict(full_features=True, full_grid=False, grid_ground=100))
+        self._test(data_args=dict(full_features=False, full_grid=False, grid_ground=self._grid_ground()))
+        self._test(data_args=dict(full_features=True, full_grid=False, grid_ground=self._grid_ground()))
 
     def test_law_tfl(self):
         self._test(data_args=dict(full_features=False, full_grid=True))
-        self._test(data_args=dict(full_features=False, full_grid=False, grid_ground=100))
-        self._test(data_args=dict(full_features=True, full_grid=False, grid_ground=100))
+        self._test(data_args=dict(full_features=False, full_grid=False, grid_ground=self._grid_ground()))
+        self._test(data_args=dict(full_features=True, full_grid=False, grid_ground=self._grid_ground()))
 
     def test_law_mt(self):
         self._test(data_args=dict(full_features=False, full_grid=True))
-        self._test(data_args=dict(full_features=False, full_grid=False, grid_ground=100))
-        self._test(data_args=dict(full_features=True, full_grid=False, grid_ground=100))
+        self._test(data_args=dict(full_features=False, full_grid=False, grid_ground=self._grid_ground()))
+        self._test(data_args=dict(full_features=True, full_grid=False, grid_ground=self._grid_ground()))